9d552c6ac08c47366d7b227053355011d4403fac
Miroslav Misik <mirom73@gmail.com>
2019-02-16 00:12:49 +0100

    finished exercise 13

--- NumeralSystems/Program.cs
+++ NumeralSystems/Program.cs
@@ -449,10 +449,53 @@ namespace NumeralSystems
             }
             Console.WriteLine("{0} in Arabic = {1} in Roman", inputConsole, resultInRoman);
             EndOfScript();
-
+            
             //13.Write a program that by given N, S, D (2 ≤ S, D ≤ 16) converts the number
             //N from an S-based numeral system to a D based numeral system. 
+            Console.WriteLine("EX13: Program that by given N, S, D (2 ≤ S, D ≤ 16) converts the number N \n" +
+                "      from an S-based numeral system to a D based numeral system.");
+            Console.Write("Enter source numeral system 2 .. 16: ");
+            int sourceNumSystem = int.Parse(Console.ReadLine());
+            if (sourceNumSystem < 2 || sourceNumSystem > 16 )
+            {
+                Console.WriteLine("Source numeral system \"{0}\" is out of the range 2 .. 16", sourceNumSystem);
+                goto End;
+            }
+            Console.Write("Enter destination numeral system 2 .. 16: ");
+            int destNumSystem = int.Parse(Console.ReadLine());
+            if (destNumSystem < 2 || destNumSystem > 16)
+            {
+                Console.WriteLine("Sestination numeral system \"{0}\" is out of the range 2 .. 16", destNumSystem);
+                goto End;
+            }
+            Console.Write("Enter number to convert: ");
+            string sourceNum = Console.ReadLine();
+            string[] arrayChars = "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F".Split(new char[] { ',' });
+            double tmpDecimal = 0;
+            string destNum = "";
 
+            //convert to decimal system, this conversion has the same rules for all system
+            for (int i = 0; i < sourceNum.Length; i++)
+            {
+                int intFromLeft = Array.IndexOf(arrayChars, sourceNum.Substring(i, 1));
+                int powerForNumSystem = sourceNum.Length - i - 1;
+                tmpDecimal += intFromLeft * Math.Pow(sourceNumSystem, powerForNumSystem);
+            }
+            
+            //convert from decimal system in to the destination numerical system
+            //this conversion has also the same rules for all destination num systems
+            while (tmpDecimal > 0)
+            {
+                int intFromLeft = (int)(tmpDecimal % destNumSystem);
+                string intFromLeftChar = arrayChars[intFromLeft];
+                destNum = destNum.Insert(0, intFromLeftChar);
+                tmpDecimal = Math.Floor(tmpDecimal / destNumSystem);
+            }
+                        
+            Console.WriteLine("{0} in {1} num system = {2} in {3} num system", sourceNum, sourceNumSystem, destNum, destNumSystem);
+            End:
+
+            EndOfScript();
             //14.Try adding up 50,000,000 times the number 0.000001.Use a loop
             //and addition(not direct multiplication). Try it with float and double and
             //after that with decimal.Do you notice the huge difference in the
